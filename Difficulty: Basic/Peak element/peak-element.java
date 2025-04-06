536ffa5396535d9e42634668e881538938dd55cd
komalrani12 <167525085+komalrani12@users.noreply.github.com>
2025-04-07 01:03:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Peak element/peak-element.java
+++ Difficulty: Basic/Peak element/peak-element.java
@@ -51,27 +51,16 @@ class GFG {
 class Solution {
 
     public int peakElement(int[] arr) {
-        int n=arr.length;
-        if(n==1){
-            return 0;
-        }
-            for(int i=0; i<n; i++){
-                if(i==0){
-                    if(arr[i]>=arr[i+1])
-                    return i;
-                }
-                else if(i==n-1){
-                    if(arr[i]>=arr[i-1])
-                    return i;
-                }
-                else{
-                   if(arr[i]>=arr[i-1]&& arr[i]>=arr[i+1])
-                   return i;
-                }
-            }
-
-        return -1;
+         int n = arr.length;
+        if(n==1) return 0;
+        if(arr[0]>arr[1]) return 0;
+        if(arr[n-1]>arr[n-2]) return n-1;
         
-                
+        for(int i=1;i<arr.length-1; i++){
+            if(arr[i-1]<arr[i]&& arr[i]>arr[i+1]){
+                return i;
+            }
+        }
+        return -1;
     }
 }
